021dfadd9833957d373077068d88a462964b138c
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-30 12:38:47 +0700

    chore: add public folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const error = require("./middleware/error.mdw");
 const sequelize = require("./config/db");
 
-app.use(express.json());
-
 const jobTitleRoute = require("./routers/jobtitle.route");
 const degreeRoute = require("./routers/degree.route");
+
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.send("API is running");
