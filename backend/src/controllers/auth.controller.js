c7cb4c35c1c065f768155e837dff28b3dd1a90b8
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-01-08 14:23:45 +0700

    mock data

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,7 +6,12 @@ exports.login = async (req, res, next) => {};
 // @desc    Đăng xuất
 // @route   POST /api/auth/logout
 // @access  Public
-exports.logout = async (req, res, next) => {};
+exports.logout = async (req, res, next) => {
+    req.ression.destroy();
+    res.json({
+        message: "Đăng xuất thành công !",
+    });
+};
 
 // @desc    Đăng ký
 // @route   POST /api/auth/register
